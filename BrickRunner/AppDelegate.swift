0615510529d800ff621e0b5638dc39d0f5c9b9b0
Christopher Ray <vde435@gmail.com>
2021-02-25 23:16:32 -0600

    Initial Commit

---
+++ BrickRunner/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BrickRunner
+//
+//  Created by Christopher Ray on 2/25/21.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
